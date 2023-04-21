6575e9141a100043df7a6a6a0479adfe637d8d40
Edson <erodstardev@gmail.com>
2023-04-21 02:21:00 -0600

    Add aplication finished

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -7,13 +7,13 @@ const questions = [
     name: "option",
     message: "What do you wanna do?",
     choices: [
-      { value: "1", name: "1. Create task" },
-      { value: "2", name: "2. List tasks" },
-      { value: "3", name: "3. List completed tasks" },
-      { value: "4", name: "4. List pending tasks" },
-      { value: "5", name: "5. Complete task(s)" },
-      { value: "6", name: "6. Delete tasks" },
-      { value: "0", name: "0. Exit" },
+      { value: "1", name: `${"1.".green} Create task` },
+      { value: "2", name: `${"2.".green} List tasks` },
+      { value: "3", name: `${"3.".green} List completed tasks` },
+      { value: "4", name: `${"4.".green} List pending tasks` },
+      { value: "5", name: `${"5.".green} Complete task(s)` },
+      { value: "6", name: `${"6.".green} Delete tasks` },
+      { value: "0", name: `${"0.".green} Exit` },
     ],
   },
 ];
@@ -47,20 +47,88 @@ const readInput = async (message) => {
       type: "input",
       name: "input",
       message,
-      // validate(value) {
-      //   if (value.length == 0) {
-      //     return "Please submit a value";
-      //   }
-      //   return value;
-      // },
+      validate(value) {
+        if (value.length == 0) {
+          return "Please submit a value";
+        }
+        return true;
+      },
     },
   ];
   const { input } = await inquirer.prompt(question);
   return input;
 };
 
+const beforeDeleteTaskPrompt = async (tasks) => {
+  const choices = tasks.map((task, i) => {
+    const idx = `${i + 1}.`.green;
+    return {
+      value: task.id,
+      name: `${idx} ${task.desc}`,
+    };
+  });
+
+  choices.unshift({
+    value: "0",
+    name: `${"0.".green} Cancel`,
+  });
+
+  const questions = [
+    {
+      type: "list",
+      name: "id",
+      message: "Delete a task",
+      choices,
+    },
+  ];
+
+  const { id } = await inquirer.prompt(questions);
+
+  return id;
+};
+
+const confirmPrompt = async () => {
+  const question = [
+    {
+      type: "confirm",
+      name: "confirmation",
+      message: "Are you sure that you want to delete this task?",
+    },
+  ];
+
+  const { confirmation } = await inquirer.prompt(question);
+  return confirmation;
+};
+
+const beforeCompleteTaskPrompt = async (tasks) => {
+  const choices = tasks.map((task, i) => {
+    const idx = `${i + 1}.`.green;
+    return {
+      value: task.id,
+      name: `${idx} ${task.desc}`,
+      checked: task.completedAt ? true : false,
+    };
+  });
+
+  const question = [
+    {
+      type: "checkbox",
+      name: "ids",
+      message: "Select a task to complete",
+      choices,
+    },
+  ];
+
+  const { ids } = await inquirer.prompt(question);
+
+  return ids;
+};
+
 module.exports = {
   inquirerMenu,
   menuStop,
   readInput,
+  beforeDeleteTaskPrompt,
+  confirmPrompt,
+  beforeCompleteTaskPrompt,
 };
--- models/tasks.js
+++ models/tasks.js
@@ -1,13 +1,75 @@
 const Task = require("./task");
 
 class Tasks {
+  get tasksArray() {
+    const tasks = [];
+    Object.keys(this._list).forEach((key) => tasks.push(this._list[key]));
+    return tasks;
+  }
+
   constructor() {
     this._list = {};
+  }
+
+  loadTasksFromArray(tasks) {
+    tasks.forEach((task) => (this._list[task.id] = task));
   }
 
   createTask(desc = "") {
     const task = new Task(desc);
     this._list[task.id] = task;
+  }
+
+  listAllTasks() {
+    Object.keys(this._list).forEach((key, i) => {
+      const { desc, completedAt } = this._list[key];
+      const idx = `${i + 1}.`.green;
+      const isCompleted = `${completedAt ? "Completed".green : "Pending".red}`;
+
+      console.log(`${idx} ${desc} :: ${isCompleted}`);
+    });
+  }
+
+  listTasksByStatus(completed) {
+    let counter = 0;
+    Object.keys(this._list).forEach((key) => {
+      const { desc, completedAt } = this._list[key];
+      const isCompleted = `${completedAt ? "Completed".green : "Pending".red}`;
+
+      if (completed) {
+        if (completedAt) {
+          counter += 1;
+          console.log(
+            `${(counter + ".").green} ${desc} :: ${completedAt.green}`
+          );
+        }
+      } else {
+        if (!completedAt) {
+          counter += 1;
+          console.log(`${(counter + ".").green} ${desc} :: ${isCompleted}`);
+        }
+      }
+    });
+  }
+
+  deleteTaskById(id) {
+    if (this._list[id]) {
+      delete this._list[id];
+    }
+  }
+
+  toggleCompletedTasks(ids) {
+    ids.forEach((id) => {
+      const task = this._list[id];
+      if (!task.completedAt) {
+        task.completedAt = new Date().toISOString();
+      }
+    });
+    this.tasksArray.forEach((task) => {
+      if (!ids.includes(task.id)) {
+        this._list[task.id].completedAt = null;
+      }
+    });
   }
 }
 
